69e8a2d2797e2a9cdd1c138e00c73a30b58ed009
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2023-12-09 11:52:37 +0800

    create components and style

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { createRoot } from "react-dom/client"
-
-const element = <h1>Hello World!</h1>
+import ContactApp from "./components/Contact";
+import "./style/style.css"
 
 const root = createRoot(document.getElementById("root"))
-root.render(element)
+root.render(<ContactApp />)
