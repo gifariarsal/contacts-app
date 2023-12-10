50396a4cdd72e6d231a744eeec0c25c1c93b1c9e
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2023-12-10 17:12:26 +0800

    add delete feature

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client"
-import ContactApp from "./components/Contact";
+import ContactApp from "./components/ContactApp";
 import "./style/style.css"
 
 const root = createRoot(document.getElementById("root"))
