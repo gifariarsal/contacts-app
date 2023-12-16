472425cc1e85b492b65a1006bd3a45ded2fdc1fd
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2023-12-16 22:21:31 +0800

    add navigation

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client"
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import ContactApp from "./components/ContactApp";
-import "./style/style.css"
+import "./style/style.css";
 
-const root = createRoot(document.getElementById("root"))
-root.render(<ContactApp />)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <ContactApp />
+  </BrowserRouter>
+);
